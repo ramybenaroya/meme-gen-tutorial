dc2caf23551664af67ed8141b03918d4a4f9c013
ramyb <rami@dudamobile.com>
2015-02-01 18:53:18 +0200

    add bootstrap

--- Brocfile.js
+++ Brocfile.js
@@ -4,6 +4,12 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
+app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {
+	destDir: 'assets'
+});
+
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
