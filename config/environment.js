b0b24649d4c3b1892c69a212e649e3cf1cc26a99
ramyb <rami@dudamobile.com>
2015-02-01 14:36:27 +0200

    add firebase url to config

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,7 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    firebase: 'https://flickering-fire-9959.firebaseio.com/memeGen',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
