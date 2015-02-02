bdbaa38e34a119af0972feb4fe4acbe8b6dc04b7
ramyb <rami@dudamobile.com>
2015-02-03 01:27:25 +0200

    simplify meme-item

--- app/components/meme-item.js
+++ app/components/meme-item.js
@@ -6,22 +6,15 @@ export default Ember.Component.extend({
 	classNames: ['meme-item'],
 	attributeBindings: ['style'],
 	editable: false,
-	contentEditableValue: function(){
-		return this.get('editable').toString();
-	}.property('editable'),
 	notEditable: Ember.computed.not('editable'),
 	style: Ember.computed.oneWay('backgroundImageStyle'),
 	backgroundImageStyle: function() {
 		return 'background-image: url(' + this.get('content.imgSrc') + ')';
 	}.property('content.imgSrc'),
 	openerStyle: function() {
-		if (this.get('_state') === 'inDOM'){
-			this.$('.opener').css('font-size', this.get('content.openerFontSize') + 'px');	
-		}
-	}.observes('content.openerFontSize').on('didInsertElement'),
+		return 'font-size: ' + this.get('content.openerFontSize') + 'px';	
+	}.property('content.openerFontSize'),
 	closerStyle: function() {
-		if (this.get('_state') === 'inDOM'){
-			this.$('.closer').css('font-size', this.get('content.closerFontSize') + 'px');
-		}
-	}.observes('content.closerFontSize').on('didInsertElement')
+		return 'font-size: ' + this.get('content.closerFontSize') + 'px';	
+	}.property('content.closerFontSize'),
 });
