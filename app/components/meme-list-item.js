3ab9dfb5430e5ded445448e4da0444ec05da99d8
ramyb <rami@dudamobile.com>
2015-02-02 20:07:07 +0200

    fix delete meme

--- app/components/meme-list-item.js
+++ app/components/meme-list-item.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	classNames: ['meme-list-item', 'panel', 'panel-default']
+	classNames: ['meme-list-item', 'panel', 'panel-default'],
+	actions: {
+		deleteMeme: function(meme){
+			this.sendAction('deleteMeme', meme);
+		}
+	}
 });
